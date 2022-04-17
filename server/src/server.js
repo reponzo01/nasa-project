6499eccca77ea62336021caa0f0bec90d8f94b9a
Emanuel Vargas <reponzo01@gmail.com>
2022-04-17 11:49:44 -0400

    163. Mongoose schemas

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const http = require('http');
 const mongoose = require('mongoose');
 
@@ -7,7 +8,7 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = "mongodb+srv://nasa-api:wqQ6Z7iSDBUfsyYe@nasacluster.76h3p.mongodb.net/nasa?retryWrites=true&w=majority"
+const MONGO_URL = `mongodb+srv://nasa-api:${process.env.MONGO_DB_USER_PASSWORD}@${process.env.MONGO_DB_SERVER}/nasa?retryWrites=true&w=majority`
 
 const server = http.createServer(app);
 
