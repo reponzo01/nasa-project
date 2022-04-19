84bea470001f6bff55d51d4c1c53de755f247d61
Emanuel Vargas <reponzo01@gmail.com>
2022-04-18 22:00:30 -0400

    184. Wrap up database

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,8 +1,9 @@
-// const launches = require('./launches.mongo');
+const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
+
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launches = new Map();
-
-let latestFlightNumber = 100;
 
 const launch = {
     flightNumber: 100,
@@ -15,39 +16,73 @@ const launch = {
     success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
-function existsLaunchWithId(launchId) {
-    return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+    return await launchesDatabase.findOne({
+        flightNumber: launchId,
+    });
 }
 
-function getAllLaunches() {
-    return Array.from(launches.values());
+async function getLatestFlightNumber() {
+    const latestLaunch = await launchesDatabase
+        .findOne()
+        .sort('-flightNumber');
+
+    if (!latestLaunch) {
+        return DEFAULT_FLIGHT_NUMBER;
+    }
+
+    return latestLaunch.flightNumber;
 }
 
-function addNewLaunch(launch) {
-    latestFlightNumber++;
-    launches.set(
-        latestFlightNumber,
-        Object.assign(launch, {
-            success: true,
-            upcoming: true,
-            customers: ['ZTM', 'NASA'],
-            flightNumber: latestFlightNumber,
-        }
-    ));
+async function getAllLaunches() {
+    return await launchesDatabase
+        .find({}, { '_id': 0, '__v': 0, });
 }
 
-function abortLaunchById(launchId) {
-    const aborted = launches.get(launchId);
-    aborted.upcoming = false;
-    aborted.success = false;
-    return aborted;
+async function saveLaunch(launch) {
+    const planet = await planets.findOne({
+        keplerName: launch.target
+    });
+
+    if (!planet) {
+        throw new Error('No matching planet was found.');
+    }
+    await launchesDatabase.findOneAndUpdate({
+        flightNumber: launch.flightNumber,
+    }, launch, {
+        upsert: true,
+    });
+}
+
+async function scheduleNewLaunch(launch) {
+    const newFlightNumber = await getLatestFlightNumber() + 1;
+    const newLaunch = Object.assign(launch, {
+        success: true,
+        upcoming: true,
+        customers: ['ZTM', 'NASA'],
+        flightNumber: newFlightNumber,
+    });
+
+    await saveLaunch(newLaunch);
+}
+
+async function abortLaunchById(launchId) {
+    console.log(`aborting ${launchId}`);
+    const aborted = await launchesDatabase.updateOne({
+        flightNumber: launchId,
+    }, {
+        upcoming: false,
+        success: false,
+    });
+
+    return aborted.modifiedCount === 1;
 }
 
 module.exports = {
     existsLaunchWithId,
     getAllLaunches,
-    addNewLaunch,
+    scheduleNewLaunch,
     abortLaunchById,
 };
---
+++ server/src/services/mongo.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const MONGO_URL = `mongodb+srv://nasa-api:${process.env.MONGO_DB_USER_PASSWORD}@${process.env.MONGO_DB_SERVER}/nasa?retryWrites=true&w=majority`
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
+async function mongoConnect() {
+    await mongoose.connect(MONGO_URL);
+}
+
+async function mongoDisconnect() {
+    await mongoose.disconnect();
+}
+
+module.exports = {
+    mongoConnect,
+    mongoDisconnect,
+};
